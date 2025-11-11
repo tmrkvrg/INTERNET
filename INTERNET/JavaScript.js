dc04d0382c7d84df0abf11cf528d12f5cc0716f9
Timur Kovriga <tmrkvrg@gmail.com>
2025-11-11 14:25:57 +0200

    javascript1

---
+++ INTERNET/JavaScript.js
@@ -0,0 +1,29 @@
+﻿document.write("something");
+alert("something");
+var num = prompt("Enter a nature number:", 7);
+var name = prompt("write your name")
+document.write("<br>");
+document.write("hello" + name);
+document.write("<br>");
+var age = parseInt(prompt("Enter your age:", 0));
+document.write("you are " + age + " years old");
+var result = age + 1;
+document.write("<br>");
+document.write("next year you will be " + result + " years old");
+document.write("<br>");
+document.write("תרגיל 1")
+document.write("<br>");
+var num1 = parseInt(prompt("Enter a nature number:", 0));
+var num2 = parseInt(prompt("Enter a nature number:", 0));
+var num3 = num1 + num2
+var num4 = num1 - num2
+var num5 = num1 * num2
+var num6 = num1 / num2
+document.write("sum:" + num3);
+document.write("<br>");
+document.write("hafresh:" + num4);
+document.write("<br>");
+document.write("mutlyplay:" + num5);
+document.write("<br>");
+document.write("devided:" + num6);
+ 
