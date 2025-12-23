f5e6d023d2d917c428eac129603c7e7129de88a7
Timur Kovriga <tmrkvrg@gmail.com>
2025-12-23 13:00:53 +0200

    טבלה

---
+++ INTERNET/calculator.js
@@ -0,0 +1,5 @@
+﻿function checkanswer()
+{
+    var num = document.getElementById("num").value;
+    alert("Number you entered is:" + num);
+}
---
+++ INTERNET/ex22.js
@@ -0,0 +1,11 @@
+﻿//var div = document.getElementById("aElement");
+//var aEl = document.createElement("a");
+//aEl.setAttribute("href", "http://www.google.com");
+//aEl.innerHTML = "GOOGLE";
+//div.appendChild(aEl);
+
+var div = document.getElementById("aElement");
+var aEl = document.createElement("img");
+aEl.setAttribute("src", "image1.jpg");
+aEl.setAttribute("width", "200");
+div.appendChild(aEl);
