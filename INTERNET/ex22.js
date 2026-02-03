8bc252622b191e1f156190b27ac5d9ce5645a49d
Timur Kovriga <tmrkvrg@gmail.com>
2026-02-03 14:22:29 +0200

    ex1

--- INTERNET/ex22.js
+++ INTERNET/ex22.js
@@ -1,11 +1,97 @@
-﻿//var div = document.getElementById("aElement");
+﻿//Targil 1
+//var div = document.getElementById("aElement");
 //var aEl = document.createElement("a");
 //aEl.setAttribute("href", "http://www.google.com");
 //aEl.innerHTML = "GOOGLE";
 //div.appendChild(aEl);
 
-var div = document.getElementById("aElement");
-var aEl = document.createElement("img");
-aEl.setAttribute("src", "image1.jpg");
-aEl.setAttribute("width", "200");
-div.appendChild(aEl);
+//Targil 2
+//var div = document.getElementById("aElement");
+//var aEl = document.createElement("img");
+//aEl.setAttribute("src", "image1.jpg");
+//aEl.setAttribute("width", "200");
+//div.appendChild(aEl);
+
+//targil 3
+//var div3 = document.getElementById("div3");
+//var table = document.createElement("table");
+//var tr = document.createElement("tr");
+//var td = document.createElement("td");
+//var td2 = document.createElement("td");
+//var img = document.createElement("img");
+//img.setAttribute("src", "img/1img.jpg");
+//img.setAttribute("width", "30");
+//img.setAttribute("height", "200 ");
+//td2.appendChild(img);
+//var link = document.createElement("a");
+//link.setAttribute("href", "http://www.google.com");
+//link.innerHTML = "GOOGLE"
+//td.appendChild(link);
+//tr.appendChild(td);
+//tr.appendChild(td2);
+//table.appendChild(tr);
+//div3.appendChild(table);
+
+//Targil 4
+//var div4 = document.getElementById("div4");
+//var table = document.createElement("table");
+//div4.appendChild(table);
+//for (var i = 1; i <= 5; i++)
+//{
+//    var tr = document.createElement("tr");
+//    var td = document.createElement("td");
+//    table.appendChild(tr);
+//    tr.appendChild(td);
+//    var image = document.createElement("img");
+//    image.setAttribute("width", "100");
+//    td.appendChild(image);
+//    image.setAttribute("src", "img/img" + i + ".jpg");
+//}
+
+//Targil 5
+//var div5 = document.getElementById("div5");
+//var table = document.createElement("table");
+//div5.appendChild(table);
+//for (var i = 1; i <= 5; i++) {
+//    var tr = document.createElement("tr");
+//    var td = document.createElement("td");
+//    table.appendChild(tr);
+//    tr.appendChild(td);
+//    var image = document.createElement("img");
+//    image.setAttribute("width", "100");
+//    image.setAttribute("src", "img/img" + i + ".jpg");
+//    td.appendChild(image);
+//    var td2 = document.createElement("td");
+//    tr.appendChild(td2);
+//    var link = document.createElement("a");
+//    link.innerHTML = "html" + i; 
+//    link.setAttribute("href", "img/img" + i + ".jpg");
+//    td2.appendChild(link);
+//}
+
+//Targil 6 and 7
+//var div5 = document.getElementById("div5");
+//var table = document.createElement("table");
+//div5.appendChild(table);
+//var num = prompt("Enter the number")
+//for (var i = 1; i <= num; i++) {
+//    var tr = document.createElement("tr");
+//    var td = document.createElement("td");
+//    table.appendChild(tr);
+//    tr.appendChild(td);
+//    var image = document.createElement("img");
+//    image.setAttribute("width", "100");
+//    image.setAttribute("src", "img/img" + i + ".jpg");
+//    td.appendChild(image);
+//    var td2 = document.createElement("td");
+//    tr.appendChild(td2);
+//    var link = document.createElement("a");
+//    link.innerHTML = "html" + i;
+//    link.setAttribute("href", "img/img" + i + ".jpg");
+//    td2.appendChild(link);
+//    if (i % 2 == 0)
+//    {
+//        tr.style.backgroundColor = "yellow";
+//    }
+//}
+
